9d6e82cde82cee40da389ff271a17865ed269916
Karol Złotowski <karol.zlotowski@teb.edu.pl>
2022-03-03 15:53:10 +0100

    v2

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -39,6 +39,8 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -134,11 +136,31 @@
             this.label7.Size = new System.Drawing.Size(0, 15);
             this.label7.TabIndex = 13;
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 189);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 15);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "delta:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(54, 189);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 15);
+            this.label9.TabIndex = 15;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(301, 220);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -170,5 +192,7 @@
         private Label label5;
         private Label label6;
         private Label label7;
+        private Label label8;
+        private Label label9;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -14,18 +14,25 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double a, b, c, ans1, ans2;
+            double a, b, c, ans1, ans2, d1;
             a = double.Parse(textBox3.Text);
             b = double.Parse(textBox2.Text);
             c = double.Parse(textBox1.Text);
             ans1 = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2  * a);
             ans2 = (-1 * b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2  * a);
+            d1 = (Math.Pow(b, 2) - 4 * a * c);
             label6.Text = ans1.ToString("n2");
             label7.Text = ans2.ToString("n2");
+            label9.Text = d1.ToString();
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
         {
 
         }
