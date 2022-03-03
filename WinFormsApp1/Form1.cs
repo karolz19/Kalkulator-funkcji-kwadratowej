576cf3ed15a7bf5b69e882c2a25777cd7d2bd3cb
3111272 <3111272@TE-GDA-D-D37H07>
2022-03-03 15:38:20 +0100

    Dodaj pliki projektów.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c, ans1, ans2;
+            a = double.Parse(textBox3.Text);
+            b = double.Parse(textBox2.Text);
+            c = double.Parse(textBox1.Text);
+            ans1 = (-1 * b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2  * a);
+            ans2 = (-1 * b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2  * a);
+            label6.Text = ans1.ToString("n2");
+            label7.Text = ans2.ToString("n2");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
